b94004745e6706aa3e53f8a1a78df7a612a1b096
Yousef <yousefm2315@gmail.com>
2025-06-30 17:42:03 -0400

    setup front end layout and variables

--- client/repo-report/src/App.jsx
+++ client/repo-report/src/App.jsx
@@ -1,10 +1,45 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [overall, setOverall] = useState('');
+  const [single, setSingle] = useState([]);
+  const [num, setNum] = useState('');
+  const [url, setUrl] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+
+    //filtering - validate
+
+
+    //do something
+
+
+  };
+
 
   return (
     <>
-      <h1>hello</h1>
+      <div className='header'>
+        <h1>Repo Report</h1>
+      </div>
+      <form className='input-field'>
+        <div>
+          <input type='number' placeholder='#' value={num} onChange={(e) => {setNum(e.target.value)}}></input>
+          <input type='text' placeholder='Github URL' value={url} onChange={(e) => {setUrl(e.target.value)}}></input>
+        </div>
+        <button type='submit' onClick={(e) => {handleSubmit(e)}}>Summerize</button>
+      </form>
+      <div className='output'>
+        <div className='overall-summary'>
+          {/* overall summary card */}
+        </div>
+        <div className='single-commit'>
+          {/* single commit summary cards */}
+        </div>
+      </div>
     </>
   )
 }
