c143614cb385f6d5059ad647048a3ace7079b2f2
Yousef <yousefm2315@gmail.com>
2025-06-30 16:52:48 -0400

    fix cors syntax errors

--- server/server/Program.cs
+++ server/server/Program.cs
@@ -3,11 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.AddCors(options => {
+builder.Services.AddCors(options => {
     options.AddDefaultPolicy(policy => {
         policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
-    })
-})
+    });
+});
 
 var app = builder.Build();
 
